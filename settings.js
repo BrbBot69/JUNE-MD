34837957bfd51f1a215aa963204bc3d8fffdca6c
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-13 18:14:17 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
-  author: 'SUPREME',
-  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  packname: '♤𝗑ᴾᴿᴼ♧',
+  author: '♤𝗑ᴾᴿᴼ♧',
+  botName: "BALLAS𝗑ᴾᴿᴼ",
+  botOwner: '♤𝗑ᴾᴿᴼ♧', // Your name
+  ownerNumber: '27767494368', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
